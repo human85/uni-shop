f7791234949075ec9cae8f1807b7fd9421576130
陈浩 <chenhao2110@qq.com>
2023-06-19 14:52:00 +0800

    完成了tabBar的开发

---
+++ pages/cart/cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <view>
+
+  </view>
+</template>
+
+<script>
+  var arr = [
+    {
+      name: "FOO",
+      value: 1.23456789
+    },
+    {
+      name: "BAR",
+      value: 2.23456789
+    },
+    {
+      name: "BAZ",
+      value: 3.23456789
+    }
+  ];
+
+  var a = { j: 123 }
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
